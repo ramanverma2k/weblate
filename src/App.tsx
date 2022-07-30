47148f04c73b950097034e8bd748cc54f2fcd460
ramanverma2k <ramanverma4183@gmail.com>
2022-07-30 23:41:45 +0530

    App.tsx: tailwind-ify and animated text gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App () {
-  const [count, setCount] = useState(0)
-
+function App() {
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='flex flex-col h-screen justify-center items-center'>
+      <h1 className='text-9xl font-extrabold bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent animate-gradient-x'>TailwindCSS</h1>
+      <h2 className='text-4xl font-semibold'> Vite + React + Typescript</h2>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      'animation': {
+        'gradient-x': 'gradient-x 5s ease infinite'
+      },
+      'keyframes': {
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        }
+      }
     },
     plugins: []
   }
