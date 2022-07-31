a5f95cbf944b326b36e87db24d02dc9e42aecd58
ramanverma2k <ramanverma4183@gmail.com>
2022-07-31 10:47:27 +0530

    replace standard with eslint
    * Typescript support out of the box.
    * Still uses standardjs eslint configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-function App() {
+function App () {
   return (
     <div className='flex flex-col h-screen justify-center items-center'>
       <h1 className='text-9xl font-extrabold bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent animate-gradient-x'>TailwindCSS</h1>
