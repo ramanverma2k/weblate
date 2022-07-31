011ae06b09849a1662f02ba30f777dfb1c0984bc
ramanverma2k <ramanverma4183@gmail.com>
2022-07-31 12:59:15 +0530

    __tests__: update App.test.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -8,6 +8,11 @@ describe('App', () => {
     expect(screen.getByText('TailwindCSS')).toBeInTheDocument()
   })
 
+  it('Hello Vite is not in the document', () => {
+    render(<App />)
+    expect(screen.queryByText('Hello Vite')).not.toBeInTheDocument()
+  })
+
   it('Vite + React + Typescript is visible', () => {
     render(<App />)
     expect(screen.getByText('Vite + React + Typescript')).toBeInTheDocument()
